e8cad54efcf99f78ac9720026330e7927fe99302
pablo barcena <pablobarcena99@gmail.com>
2022-10-25 13:01:16 +0200

    spa

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename='mendaweb'>
+  <BrowserRouter basename='mendalerenda'>
     <App />
   </BrowserRouter>
 );
