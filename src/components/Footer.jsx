815f35c860bce0877a05c4a2c9a61f0092cd8bb4
Pablo Barcena Carrion <pablobarcena99@MacBook-Pro-de-Pablo-2.local>
2023-04-17 20:14:41 +0200

    Ready to deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ import {
 import { Col, Container, Row } from "react-bootstrap";
 
 const SocialLink = styled.a`
-  font-size: 30px;
+  font-size: 20px;
   color: ${(props) => props.color} !important;
   padding: 4px;
 `;
@@ -25,176 +25,90 @@ const FooterTitle = styled.h6`
 `;
 
 const FooterContainer = styled.div`
-  min-height: 300px;
+  min-height: 500px;
   background-color: #141414;
   p {
     color: white;
     font-family: var(--font-roboto);
   }
-  h6{
+  h6 {
     font-family: var(--font-dxsitrus);
   }
 `;
 const FooterSection = styled.div`
+  h6 {
+    font-size: calc(1.2rem + 1vw);
+  }
   display: flex;
-  justify-content: flex-end;
-  padding: 30px;
+  justify-content: ;
+  align-items: ${(props) => props.align};
+  flex-direction: column;
+  padding: 30px 0px 30px 0px;
+
+  @media (max-width: 992px) {
+    align-items: center;
+  }
 `;
+
 export default class Footer extends Component {
   render() {
     return (
       <>
-        {/* <FooterContainer>
+        <FooterContainer>
           <Container>
             <Row>
-              <Col lg={6}>
-                <h6>MENDA LERENDA</h6>
-                <p> mendalerendacontacto@gmail.com </p>
-                <p>
-                  {" "}
-                  <FaPhoneAlt /> +34 664 292 833{" "}
-                </p>
-                <p>
-                  {" "}
-                  <FaPhoneAlt /> +34 617 146 287{" "}
-                </p>
+              <Col lg={6} className=''>
+                <FooterSection>
+                  <h6>MENDA LERENDA</h6>
+                  <p> mendalerendacontacto@gmail.com </p>
+                  <p> +34 664 292 833 </p>
+                  <p> +34 617 146 287 </p>
+                </FooterSection>
               </Col>
               <Col lg={6}>
-                <FooterSection>
-                  <SocialLink
-                    target='_blank'
-                    href='https://twitter.com/mendalerenda__?s=20'
-                    className='me-4 link-secondary'
-                    color={"#2D9BF0"}>
-                    <FaTwitter />
-                  </SocialLink>
-                  <SocialLink
-                    target='_blank'
-                    href='https://www.youtube.com/channel/UCq4gAcyctpsPZqVtEG3nRXA'
-                    className='me-4 link-secondary'
-                    color={"#F82502"}>
-                    <FaYoutube />
-                  </SocialLink>
-                  <SocialLink
-                    target='_blank'
-                    href='https://instagram.com/mendalerenda_?igshid=YmMyMTA2M2Y=applew '
-                    className='me-4 link-secondary'
-                    color={"#C0245E"}>
-                    <FaInstagram />
-                  </SocialLink>
-                  <SocialLink
-                    target='_blank'
-                    href='https://music.apple.com/us/artist/menda-lerenda/1515026644'
-                    className='me-4 link-secondary'
-                    color={"white"}>
-                    <FaApple />
-                  </SocialLink>
-                  <SocialLink
-                    target='_blank'
-                    href='https://open.spotify.com/artist/4lAJspyHzaPknkPilMELFu?si=-9XLj3RYR12KSfivALAB_Q'
-                    className='me-4 link-secondary'
-                    color={"#1CD761"}>
-                    <FaSpotify />
-                  </SocialLink>
+                <FooterSection align={"flex-end"}>
+                  <div>
+                    <SocialLink
+                      target='_blank'
+                      href='https://twitter.com/mendalerenda__?s=20'
+                      className='me-4 link-secondary'
+                      color={"#2D9BF0"}>
+                      <FaTwitter />
+                    </SocialLink>
+                    <SocialLink
+                      target='_blank'
+                      href='https://www.youtube.com/channel/UCq4gAcyctpsPZqVtEG3nRXA'
+                      className='me-4 link-secondary'
+                      color={"#F82502"}>
+                      <FaYoutube />
+                    </SocialLink>
+                    <SocialLink
+                      target='_blank'
+                      href='https://instagram.com/mendalerenda_?igshid=YmMyMTA2M2Y=applew '
+                      className='me-4 link-secondary'
+                      color={"#C0245E"}>
+                      <FaInstagram />
+                    </SocialLink>
+                    <SocialLink
+                      target='_blank'
+                      href='https://music.apple.com/us/artist/menda-lerenda/1515026644'
+                      className='me-4 link-secondary'
+                      color={"white"}>
+                      <FaApple />
+                    </SocialLink>
+                    <SocialLink
+                      target='_blank'
+                      href='https://open.spotify.com/artist/4lAJspyHzaPknkPilMELFu?si=-9XLj3RYR12KSfivALAB_Q'
+                      className='me-4 link-secondary'
+                      color={"#1CD761"}>
+                      <FaSpotify />
+                    </SocialLink>
+                  </div>
                 </FooterSection>
               </Col>
             </Row>
           </Container>
-        </FooterContainer> */}
-        <footer className='text-center text-lg-start text-muted'>
-          <section className='container d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-            <div className='me-5 d-none d-lg-block'>
-              <span>Conecta con nosotros en nuestras redes sociales:</span>
-            </div>
-            <div>
-              {/* <SocialLink href='#face' className='me-4 link-secondary' color={"#3E62A9"}>
-                <FaFacebook />
-              </SocialLink> */}
-              <SocialLink
-                target='_blank'
-                href='https://twitter.com/mendalerenda__?s=20'
-                className='me-4 link-secondary'
-                color={"#2D9BF0"}>
-                <FaTwitter />
-              </SocialLink>
-              <SocialLink
-                target='_blank'
-                href='https://www.youtube.com/channel/UCq4gAcyctpsPZqVtEG3nRXA'
-                className='me-4 link-secondary'
-                color={"#F82502"}>
-                <FaYoutube />
-              </SocialLink>
-              <SocialLink
-                target='_blank'
-                href='https://instagram.com/mendalerenda_?igshid=YmMyMTA2M2Y=applew '
-                className='me-4 link-secondary'
-                color={"#C0245E"}>
-                <FaInstagram />
-              </SocialLink>
-              <SocialLink
-                target='_blank'
-                href='https://music.apple.com/us/artist/menda-lerenda/1515026644'
-                className='me-4 link-secondary'
-                color={"#141414"}>
-                <FaApple />
-              </SocialLink>
-              <SocialLink
-                target='_blank'
-                href='https://open.spotify.com/artist/4lAJspyHzaPknkPilMELFu?si=-9XLj3RYR12KSfivALAB_Q'
-                className='me-4 link-secondary'
-                color={"#1CD761"}>
-                <FaSpotify />
-              </SocialLink>
-            </div>
-          </section>
-          <section className=''>
-            <div className='container text-center text-md-start mt-5'>
-              <div className='row mt-3'>
-                <div className='col-md-3 col-lg-4 col-xl-3 mx-auto mb-4'>
-                  <FooterTitle className='text-uppercase fw-bold mb-4'> MENDA LERENDA </FooterTitle>
-                </div>
-
-                <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
-                  {/* <!-- Links --> */}
-                  <FooterTitle className='text-uppercase fw-bold mb-4'>Useful links</FooterTitle>
-                  <p>
-                    <a href='#!' className='text-reset'>
-                      Pricing
-                    </a>
-                  </p>
-                  <p>
-                    <a href='#!' className='text-reset'>
-                      Settings
-                    </a>
-                  </p>
-                  <p>
-                    <a href='#!' className='text-reset'>
-                      Orders
-                    </a>
-                  </p>
-                  <p>
-                    <a href='#!' className='text-reset'>
-                      Help
-                    </a>
-                  </p>
-                </div>
-                <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4'>
-                  <FooterTitle className='text-uppercase fw-bold mb-4'>Contacto</FooterTitle>
-
-                  <p> mendalerendacontacto@gmail.com </p>
-                  <p>
-                    {" "}
-                    <FaPhoneAlt /> +34 664 292 833{" "}
-                  </p>
-                  <p>
-                    {" "}
-                    <FaPhoneAlt /> +34 617 146 287{" "}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </section>
-        </footer>
+        </FooterContainer>
       </>
     );
   }
